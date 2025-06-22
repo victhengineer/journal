73bf43ff95c663e10363865d0e2f669eebfcf4ee
victor <victorkipchumba039@gmail.com>
2025-06-22 07:54:42 +0300

    Practice: Complete turtle shell

---
+++ 2025-06-21/python/cmd/spiral.cmd
@@ -0,0 +1,31 @@
+position
+heading
+reset
+circle 10
+right 20
+circle 20
+right 20
+circle 30
+right 20
+circle 40
+right 20
+circle 50
+right 20
+circle 60
+right 20
+circle 70
+right 20
+circle 80
+right 20
+circle 90
+right 20
+circle 100
+right 20
+circle 120
+right 20
+circle 140
+right 20
+circle 160
+heading
+forward 100
+right 90
